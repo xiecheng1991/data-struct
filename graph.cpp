5da5433970ac4171dac8f71396e876d9634f0182
谢成 <xiecheng@xiechengdeAir.lan>
2019-03-27 00:02:37 +0800

    增加图的dfs优先算法的伪代码

--- graph.cpp
+++ graph.cpp
@@ -33,6 +33,14 @@ class CGraph {
 
         //bfs遍历 广度优先 遍历图
         void bfs ( int s, int t );
+
+        //dfs遍历 深度优先 遍历图
+        void dfs( int s, int t );
+
+        void dfshelper( int s, int t, map<int,int>& pre, vector<int>& visited ) {
+            
+        }
+
     private:
         int vertexnum;         //图的顶点数量
         vector<vertex*>  vertexInfo; //图的各个顶点所能到达顶点
@@ -43,6 +51,19 @@ void printpath( map<int, int>& pre, int s, int t ) {
         printpath( pre, s, pre[t] );
     }
     cout << t << "->";
+}
+
+void CGraph::dfs( int s, int t ) {
+    map<int, int> pre;
+    for ( int i = 1; i <= vertexnum; ++i ) {
+        pre[i] = -1;
+    }
+    vector<int> visited( vertexnum + 1, false );
+    vertex* ptemp = vertexInfo[s];
+    while ( ptemp ) {
+        pre[ptemp->val] = s;
+        helper( ptemp->val, t )
+    }
 }
 
 void CGraph::bfs( int s, int t ) {
