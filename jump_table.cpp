c9cd56b1ab79aff433da8aebb91e44e1e98d4d03
ubuntu <ubuntu@localhost.localdomain>
2020-03-11 15:54:49 +0800

    完善jump_table功能代码

--- jump_table.cpp
+++ jump_table.cpp
@@ -11,50 +11,156 @@ using namespace std;
 int random_level() {
     int level = 0;
     for ( int i = 0; i < MAX_LEVEL; ++i ) {
-        if ( random( 0, 1 ) ) {
+        if ( random() % 100 < 50 ) {
             level++;
         }
     }
     return level;
 }
-
 class Node {
     public:
         Node( int val ) {
             this->val = val;
         }
         ~Node();
-    private:
         int val;                    //当前结点元素值
         Node* front[MAX_LEVEL];     //保存它在索引层中的后一个元素
 };
 
 
-
 class Jump_Table {
     public:
-    Jump_Table() {
-        head = new Node(-1);
+    Jump_Table( int val ) {
+        head = new Node( val );
         max_level = 0;
-        max_level_nodes = 0;
     }
     ~Jump_Table();
 
     //插入元素
     void insert( int val );
 
-    private:
-        Node* head;
-        int max_level;              //当前用到的最大索引层数
-        int max_level_nodes;        //当前用到的最大索引层中的索引结点数量
+	void find( int val );
+
+	void del( int val );
+
+	void Print();
+
+	Node* head;
+	int max_level;              //当前用到的最大索引层数
+//	int max_level_nodes;        //当前用到的最大索引层中的索引结点数量
 };
+
+void
+Jump_Table::Print(){
+	Node* p = head;
+	while( p ) {
+		printf( "%d ", p->val );
+		p = p->front[0];
+	}
+}
 
 void Jump_Table::insert( int val ) {
     Node* node = new Node( val );
-    int level = randomlev
+    int level = random_level();
+	Node** update = ( Node** ) malloc( sizeof(Node*) * level );
+	Node* p = head;
+	//找好没一层的前驱结点
+	for ( int i = level - 1; i >= 0; --i ) {
+		//建立几层索引
+		while( p->front[i] && p->front[i]->val < val ) {
+			p = p->front[i];
+		}
+		update[i] = p;
+	}
+	
+	//把新加入的结点插入到索引层中
+	for ( int i = 0; i < level; ++i ) {
+		Node* temp = update[i]->front[i];
+		update[i]->front[i] = node;
+		node->front[i] = temp;
+	}
+	
+	if ( level > max_level ) {
+		max_level = level;
+	}
+}
+
+void 
+Jump_Table::find( int val ) {
+	Node* p = head;
+
+	if ( p->val == val ) {
+		printf( "find it:%d\n", p->val );
+		return;
+	}
+
+	for ( int i = max_level - 1; i >= 0; --i ) {
+		while( p->front[i] && p->front[i]->val < val ) {
+			p = p->front[i];
+		}
+	}
+	if ( p && p->val == val ) {
+		printf( "find it:%d\n", p->val );
+		return;
+	}
+	printf( "not find it\n" );
+}
+
+void 
+Jump_Table::del( int val ) {
+	Node* p = head;
+	
+	if ( p->val == val ) {
+		//删除的是头结点
+		head = head->front[0];
+		//释放掉这个p结点
+		free( p );
+	}	
+
+	Node** update = ( Node** ) malloc( sizeof(Node*) * max_level );
+	//找出所有的前驱结点
+	for ( int i = max_level - 1; i >= 0; --i ) {
+		while( p->front[i] && p->front[i]->val < val ) {
+			p = p->front[i];
+		}
+		update[i] = p;
+	}
+
+	//执行删除逻辑
+	for ( int i = max_level - 1; i >= 0; --i ) {
+		if ( update[i]->front[i] && update[i]->front[i]->val == val ) {
+			update[i]->front[i] = update[i]->front[i]->front[i];
+		}
+	}
+
+	printf( "删除完成\n" );
+	return;
 }
 
 
+
 int main( int argc, char* argv[] ) {
+	Jump_Table* p = new Jump_Table( 0 );
+	for ( int i = 1; i < 5; ++i ) {
+		p->insert( i );
+	}
+	
+//删除中间结点的情况
+/*
+	printf( "find 3" );
+	p->find( 3 );
+
+	printf( "del 3" );
+	p->del( 3 );
+
+	printf( "find 3" );
+	p->find( 3 );
+*/
+
+	//删除头结点的情况
+
+	printf( "del 0\n" );
+	p->del( 0 );
+	p->Print();
     return 0;
 }
